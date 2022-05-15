2fa8b7fba3eb86c816a31d6d73e137322e8b5d8b
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-05-15 12:31:31 +0200

    OurWork css

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,11 +1,55 @@
 import React from "react";
+import athlete from "../img/athlete-small.png";
+import racer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const OurWork = () => {
   return (
-    <div>
-      <h1>Our Work</h1>
-    </div>
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <img src={athlete} alt="Athlete" />
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <img src={racer} alt="Racer" />
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <img src={goodtimes} alt="Goodtimes" />
+      </Movie>
+    </Work>
   );
 };
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+
+  h2 {
+    padding: 1rem 0;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background-color: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default OurWork;
